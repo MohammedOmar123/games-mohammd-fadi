c6497fb5ee723856f71a6722a0646bd52a2bb4da
MohammedOmar123 <mohammedbalousha60@gmail.com>
2022-08-16 21:51:03 +0300

    serve public static files into the server and create an input for search in the home page related #4

--- src/app.js
+++ src/app.js
@@ -12,7 +12,7 @@ app.use(compression());
 app.use(helmet());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-
+app.use(express.static(path.join(__dirname,'../public')));
 app.use(router);
 
 module.exports = app;
