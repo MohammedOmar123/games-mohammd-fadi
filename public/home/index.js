2c299baf9a17ae033dce35583296778871d4f51a
MohammedOmar123 <mohammedbalousha60@gmail.com>
2022-08-17 10:07:18 +0300

    save some files

--- public/home/index.js
+++ public/home/index.js
@@ -1,4 +1,31 @@
-const image = document.querySelector('.img');
-const url = "https://www.freetogame.com/g/517/thumbnail.jpg";
+const shooterSection = document.querySelector('.shooter');
+const strategySection = document.querySelector('.strategy');
+const racingSection = document.querySelector('.racing');
 
-image.setAttribute('src', url);
+const createGamesViews = (game,section) => {
+  const div = document.createElement('div');
+  const p = document.createElement('p');
+  const img = document.createElement('img');
+  // add content
+  p.textContent = game.title;
+  img.src = game.thumbnail;
+
+  // add classes
+  div.classList.add('card');
+
+  // appending
+  div.append(img, p);
+  section.append(div);
+};
+const requestGamesCategory = (categoryName, section) => {
+  fetch(`/category/${categoryName}`).then((data) => data.json()).then((games) => {
+    const data = games.slice(0, 7);
+    data.map((game) => {
+      createGamesViews(game, section);
+    });
+  });
+};
+
+requestGamesCategory('strategy', strategySection);
+requestGamesCategory('shooter', shooterSection);
+requestGamesCategory('racing', racingSection);
