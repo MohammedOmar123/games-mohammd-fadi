43d94afba7f792bcb23fe8d9b3a751f2096f59ea
MohammedOmar123 <mohammedbalousha60@gmail.com>
2022-08-17 12:03:00 +0300

    solve conflicts

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,5 +1,12 @@
 const getGamesFromCategory = require('./getGamesFromCategory');
 const getSearchValue = require('./search');
+const { getGame, gamePage } = require('./game');
 const getLatestReleaseGames = require('./getLatestReleaseGames');
 
-module.exports = { getGamesFromCategory, getSearchValue, getLatestReleaseGames };
+module.exports = {
+  getSearchValue,
+  getGame,
+  gamePage,
+  getGamesFromCategory,
+  getLatestReleaseGames
+};
--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,9 @@
 const router = require('express').Router();
 
-const { getGamesFromCategory, getSearchValue, getLatestReleaseGames } = require('./controllers');
-
+const { getGamesFromCategory, getSearchValue, getGame, gamePage, getLatestReleaseGames } = require('./controllers');
 router.post('/handel-search', getSearchValue);
+router.post('/game', getGame);
+router.get('/game/:id', gamePage);
 router.get('/category/:categoryName', getGamesFromCategory);
 router.get('/games/latestRelease', getLatestReleaseGames);
 
